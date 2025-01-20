6b070958e77beb537e9fa2317115d83bcea112cc
Eternatus <mr.drdioyt@gmail.com>
2025-01-20 23:23:52 +0500

    Deploy to GitHub Pages

---
+++ assets/ChatDefault-ClJgaCJE.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as t}from"./index-B_YhoeON.js";const n={};function o(r,a){return t(),c("div",null," Выберите пользователя ")}const _=e(n,[["render",o]]);export{_ as default};
---
+++ assets/errors-ptkwsH4S.js
@@ -0,0 +1,1 @@
+import{d as e,r as o}from"./index-B_YhoeON.js";const u=e("errorsUser",()=>{const r=o("");return{getErrors:()=>r.value,setErrors:s=>{r.value=s},clearErrors:()=>{r.value=""}}});export{u};
