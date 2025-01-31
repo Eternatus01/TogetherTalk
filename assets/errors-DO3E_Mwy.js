8bb885a28c3db875c85497352012d3800cd803fd
Eternatus <mr.drdioyt@gmail.com>
2025-01-31 23:24:42 +0500

    Deploy to GitHub Pages

---
+++ assets/errors-DO3E_Mwy.js
@@ -0,0 +1,1 @@
+import{f as e,r as o}from"./index-CpXpfZ6u.js";const u=e("errorsUser",()=>{const r=o("");return{getErrors:()=>r.value,setErrors:s=>{r.value=s},clearErrors:()=>{r.value=""}}});export{u};
