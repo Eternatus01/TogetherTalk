0e06ebd7ef94ecf96696b7452fa5987d839a7d15
Eternatus01 <Eternatus01@users.noreply.github.com>
2025-01-31 19:57:12 +0000

    deploy: 5c77b084d0291df263cac4794fc9c96328a2d678

--- assets/errors-Dj1Nw3v4.js
+++
@@ -1,1 +0,0 @@
-import{f as e,r as o}from"./index-BSYZ5ZA2.js";const u=e("errorsUser",()=>{const r=o("");return{getErrors:()=>r.value,setErrors:s=>{r.value=s},clearErrors:()=>{r.value=""}}});export{u};
