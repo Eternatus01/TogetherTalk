660a24b3ff111333176a676fd982de5bcaf736a9
Eternatus01 <Eternatus01@users.noreply.github.com>
2025-01-31 18:53:42 +0000

    deploy: d1aaec388acab1777918fae81173bca4dfebde34

--- assets/errors-DO3E_Mwy.js
+++
@@ -1,1 +0,0 @@
-import{f as e,r as o}from"./index-CpXpfZ6u.js";const u=e("errorsUser",()=>{const r=o("");return{getErrors:()=>r.value,setErrors:s=>{r.value=s},clearErrors:()=>{r.value=""}}});export{u};
